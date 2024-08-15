5426b0e58f3c988fd476158abc2c88f9d728657d
ZweNaingOo <140169123+ZweNaingOo@users.noreply.github.com>
2024-08-15 10:27:06 +0630

    BArnya

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html'
+  selector: 'app-root',
+  templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
 
-    constructor(private primengConfig: PrimeNGConfig) { }
+  constructor(private primengConfig: PrimeNGConfig) { }
 
-    ngOnInit() {
-        this.primengConfig.ripple = true;
-    }
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
+
+  ngAfterViewInit() {
+    this.hideSpinner();
+  }
+
+  hideSpinner() {
+    setTimeout(() => {
+      const spinner = document.getElementById('nb-global-spinner');
+      if (spinner) {
+        spinner.style.display = 'none';
+      }
+    }, 1500);
+  }
 }
