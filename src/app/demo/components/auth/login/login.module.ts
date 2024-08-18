d4ee06650fcc159d60b951f7b78b3b76d8a5c1a0
ZweNaingOo <140169123+ZweNaingOo@users.noreply.github.com>
2024-08-18 21:26:43 +0630

    update code

--- src/app/demo/components/auth/login/login.module.ts
+++ src/app/demo/components/auth/login/login.module.ts
@@ -7,6 +7,7 @@ import { CheckboxModule } from 'primeng/checkbox';
 import { FormsModule } from '@angular/forms';
 import { PasswordModule } from 'primeng/password';
 import { InputTextModule } from 'primeng/inputtext';
+import { ToastModule } from 'primeng/toast';
 
 @NgModule({
     imports: [
